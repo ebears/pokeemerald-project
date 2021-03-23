91164a8bdf4730e0dd4eab77e7fb49d0027edc0e
ebears <thomlbmclean@gmail.com>
2021-03-23 03:30:19 -0400

    Reverted ability to receive items in plurals due to overwriting Item Descriptions

--- src/item.c
+++ src/item.c
@@ -101,16 +101,21 @@ void CopyItemName(u16 itemId, u8 *dst)
     StringCopy(dst, ItemId_GetName(itemId));
 }
 
-static const u8 sText_s[] = _("s");
 void CopyItemNameHandlePlural(u16 itemId, u8 *dst, u32 quantity)
 {
-    StringCopy(dst, ItemId_GetName(itemId));
-    if (quantity > 1)
+    if (itemId == ITEM_POKE_BALL)
     {
-        if (ItemId_GetPocket(itemId) == POCKET_BERRIES)
-            GetBerryCountString(dst, gBerries[itemId - ITEM_CHERI_BERRY].name, quantity);
+        if (quantity < 2)
+            StringCopy(dst, ItemId_GetName(ITEM_POKE_BALL));
         else
-            StringAppend(dst, sText_s);
+            StringCopy(dst, gText_PokeBalls);
+    }
+    else
+    {
+        if (itemId >= FIRST_BERRY_INDEX && itemId <= LAST_BERRY_INDEX)
+            GetBerryCountString(dst, gBerries[itemId - FIRST_BERRY_INDEX].name, quantity);
+        else
+            StringCopy(dst, ItemId_GetName(itemId));
     }
 }
 
